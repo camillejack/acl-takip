b611be128485438bbda857dc153cfe0fa9f7cee4
camillejack <69893098+camillejack@users.noreply.github.com>
2026-05-10 12:49:26 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+// Service Worker devre disi
+self.addEventListener("install", ()=>self.skipWaiting());
+self.addEventListener("activate", e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
