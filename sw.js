03bf9f850d1b9a94d835180cb95f3d219c2d1ea0
camillejack <69893098+camillejack@users.noreply.github.com>
2026-05-18 00:48:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - ACL Depo Yönetim Sistemi
-const CACHE = 'acl-takip-v59';
+const CACHE = 'acl-takip-v60';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', function(e) {
